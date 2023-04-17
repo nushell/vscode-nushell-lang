c1bd6ba686231f398a99798fede50b09b3f2ac3b
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2023-04-17 12:00:50 -0500

    add timing to more server functions (#105)

--- server/src/server.ts
+++ server/src/server.ts
@@ -429,126 +429,132 @@ async function runCompiler(
 }
 
 connection.onHover(async (request: HoverParams) => {
-  const document = documents.get(request.textDocument.uri);
-  const settings = await getDocumentSettings(request.textDocument.uri);
-
-  const text = document?.getText();
-
-  if (!(typeof text == "string")) return null;
-
-  // connection.console.log("request: ");
-  // connection.console.log(request.textDocument.uri);
-  // connection.console.log("index: " + convertPosition(request.position, text));
-  const stdout = await runCompiler(
-    text,
-    "--ide-hover " + convertPosition(request.position, text),
-    settings,
-    request.textDocument.uri
-  );
-
-  const lines = stdout.split("\n").filter((l) => l.length > 0);
-  for (const line of lines) {
-    const obj = JSON.parse(line);
-    // connection.console.log("hovering");
-    // connection.console.log(obj);
-
-    // FIXME: Figure out how to import `vscode` package in server.ts without
-    // getting runtime import errors to remove this deprecation warning.
-    const contents = {
-      value: obj.hover,
-      language: "nushell",
-    };
-
-    if (obj.hover != "") {
-      if (obj.span) {
-        const lineBreaks = findLineBreaks(
-          obj.file
-            ? (await fs.promises.readFile(obj.file)).toString()
-            : document?.getText() ?? ""
-        );
-
-        return {
-          contents,
-          range: {
-            start: convertSpan(obj.span.start, lineBreaks),
-            end: convertSpan(obj.span.end, lineBreaks),
-          },
-        };
-      } else {
-        return { contents };
-      }
-    }
-  }
-});
-
-// This handler provides the initial list of the completion items.
-connection.onCompletion(
-  async (request: TextDocumentPositionParams): Promise<CompletionItem[]> => {
-    // The pass parameter contains the position of the text document in
-    // which code complete got requested. For the example we ignore this
-    // info and always provide the same completion items.
-
+  return await durationLogWrapper(`onHover`, async () => {
     const document = documents.get(request.textDocument.uri);
     const settings = await getDocumentSettings(request.textDocument.uri);
 
     const text = document?.getText();
 
-    if (typeof text == "string") {
-      // connection.console.log("completion request: ");
-      // connection.console.log(request.textDocument.uri);
-      const index = convertPosition(request.position, text);
-      // connection.console.log("index: " + index);
-      const stdout = await runCompiler(
-        text,
-        "--ide-complete " + index,
-        settings,
-        request.textDocument.uri
-      );
-      // connection.console.log("got: " + stdout);
+    if (!(typeof text == "string")) return null;
 
-      const lines = stdout.split("\n").filter((l) => l.length > 0);
-      for (const line of lines) {
-        const obj = JSON.parse(line);
-        // connection.console.log("completions");
-        // connection.console.log(obj);
+    // connection.console.log("request: ");
+    // connection.console.log(request.textDocument.uri);
+    // connection.console.log("index: " + convertPosition(request.position, text));
+    const stdout = await runCompiler(
+      text,
+      "--ide-hover " + convertPosition(request.position, text),
+      settings,
+      request.textDocument.uri
+    );
 
-        const output = [];
-        let index = 1;
-        for (const completion of obj.completions) {
-          output.push({
-            label: completion,
-            kind: completion.includes("(")
-              ? CompletionItemKind.Function
-              : CompletionItemKind.Field,
-            data: index,
-          });
-          index++;
+    const lines = stdout.split("\n").filter((l) => l.length > 0);
+    for (const line of lines) {
+      const obj = JSON.parse(line);
+      // connection.console.log("hovering");
+      // connection.console.log(obj);
+
+      // FIXME: Figure out how to import `vscode` package in server.ts without
+      // getting runtime import errors to remove this deprecation warning.
+      const contents = {
+        value: obj.hover,
+        language: "nushell",
+      };
+
+      if (obj.hover != "") {
+        if (obj.span) {
+          const lineBreaks = findLineBreaks(
+            obj.file
+              ? (await fs.promises.readFile(obj.file)).toString()
+              : document?.getText() ?? ""
+          );
+
+          return {
+            contents,
+            range: {
+              start: convertSpan(obj.span.start, lineBreaks),
+              end: convertSpan(obj.span.end, lineBreaks),
+            },
+          };
+        } else {
+          return { contents };
         }
-        return output;
       }
     }
+  });
+});
 
-    return [];
+// This handler provides the initial list of the completion items.
+connection.onCompletion(
+  async (request: TextDocumentPositionParams): Promise<CompletionItem[]> => {
+    return await durationLogWrapper(`onCompletion`, async () => {
+      // The pass parameter contains the position of the text document in
+      // which code complete got requested. For the example we ignore this
+      // info and always provide the same completion items.
+
+      const document = documents.get(request.textDocument.uri);
+      const settings = await getDocumentSettings(request.textDocument.uri);
+
+      const text = document?.getText();
+
+      if (typeof text == "string") {
+        // connection.console.log("completion request: ");
+        // connection.console.log(request.textDocument.uri);
+        const index = convertPosition(request.position, text);
+        // connection.console.log("index: " + index);
+        const stdout = await runCompiler(
+          text,
+          "--ide-complete " + index,
+          settings,
+          request.textDocument.uri
+        );
+        // connection.console.log("got: " + stdout);
+
+        const lines = stdout.split("\n").filter((l) => l.length > 0);
+        for (const line of lines) {
+          const obj = JSON.parse(line);
+          // connection.console.log("completions");
+          // connection.console.log(obj);
+
+          const output = [];
+          let index = 1;
+          for (const completion of obj.completions) {
+            output.push({
+              label: completion,
+              kind: completion.includes("(")
+                ? CompletionItemKind.Function
+                : CompletionItemKind.Field,
+              data: index,
+            });
+            index++;
+          }
+          return output;
+        }
+      }
+
+      return [];
+    });
   }
 );
 
 connection.onDefinition(async (request) => {
-  const document = documents.get(request.textDocument.uri);
-  if (!document) return;
-  const settings = await getDocumentSettings(request.textDocument.uri);
+  return await durationLogWrapper(`onDefinition`, async () => {
+    const document = documents.get(request.textDocument.uri);
+    if (!document) return;
+    const settings = await getDocumentSettings(request.textDocument.uri);
 
-  const text = document.getText();
+    const text = document.getText();
 
-  // connection.console.log("request: ");
-  // connection.console.log(request.textDocument.uri);
-  // connection.console.log("index: " + convertPosition(request.position, text));
-  const stdout = await runCompiler(
-    text,
-    "--ide-goto-def " + convertPosition(request.position, text),
-    settings,
-    request.textDocument.uri
-  );
-  return goToDefinition(document, stdout);
+    // connection.console.log("request: ");
+    // connection.console.log(request.textDocument.uri);
+    // connection.console.log("index: " + convertPosition(request.position, text));
+    const stdout = await runCompiler(
+      text,
+      "--ide-goto-def " + convertPosition(request.position, text),
+      settings,
+      request.textDocument.uri
+    );
+    return goToDefinition(document, stdout);
+  });
 });
 
 // This handler resolves additional information for the item selected in
