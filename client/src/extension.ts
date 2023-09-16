8f4af9cf5bcae5af92af8314bce015dc7e1a294f
nerditation <nerditation@outlook.com>
2023-09-16 23:40:46 +0800

    add detailed error message on failure.
    the error message also guide the user to install `nushell` from website and reload vscode.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -78,12 +78,33 @@ export function activate(context: vscode.ExtensionContext) {
           console.log(
             "Nushell not found in env:PATH or any of the heuristic locations."
           );
+          // use an async arrow funciton to use `await` inside
+          return (async () => {
+            if (
+              (await vscode.window.showErrorMessage(
+                "We cannot find a nushell executable in your path or pre-defined locations",
+                "install from website"
+              )) &&
+              (await vscode.env.openExternal(
+                vscode.Uri.parse("https://www.nushell.sh/")
+              )) &&
+              (await vscode.window.showInformationMessage(
+                "after you install nushell, you might need to reload vscode",
+                "reload now"
+              ))
+            ) {
+              vscode.commands.executeCommand("workbench.action.reloadWindow");
+            }
+            // user has already seen error messages, but they didn't click through
+            // return a promise that never resolve to supress the confusing error
+            return await new Promise(() => {});
+          })();
         }
 
         return {
           options: {
             name: "Nushell",
-            shellPath: found_nushell_path || "nu",
+            shellPath: found_nushell_path,
             iconPath: vscode.Uri.joinPath(
               context.extensionUri,
               "assets/nu.svg"
