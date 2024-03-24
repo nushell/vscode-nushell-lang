98ba7cb3982fccb5713d8cc2640a298b107a9b46
Auca Coyan <aucacoyan@gmail.com>
2024-03-24 20:23:45 -0300

    :coffin: remove unused definitions

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -149,45 +149,6 @@ export function activate(context: vscode.ExtensionContext): void {
     context,
     undefined,
   );
-
-  // The debug options for the server
-  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-  const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
-
-  // If the extension is launched in debug mode then the debug server options are used
-  // Otherwise the run options are used
-  const serverOptions: ServerOptions = {
-    run: { module: serverModule, transport: TransportKind.ipc },
-    debug: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-      options: debugOptions,
-    },
-  };
-
-  // Options to control the language client
-  const clientOptions: LanguageClientOptions = {
-    // Register the server for plain text documents
-    documentSelector: [{ scheme: "file", language: "nushell" }],
-    synchronize: {
-      // Notify the server about file changes to '.clientrc files contained in the workspace
-      fileEvents: vscode.workspace.createFileSystemWatcher("**/.clientrc"),
-    },
-    markdown: {
-      isTrusted: true
-    }
-  };
-
-  // Create the language client and start the client.
-  client = new LanguageClient(
-    "nushellLanguageServer",
-    "Nushell Language Server",
-    serverOptions,
-    clientOptions
-  );
-
-  // Start the client. This will also launch the server
-  client.start();
 }
 
 export function deactivate(): void {
